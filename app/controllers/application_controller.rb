b06da93515d29b20211a56b4d47eff747425b122
github username <github email address>
2019-07-10 15:28:50 -0400

    Add helper methods logged_in? and current_user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,4 +13,13 @@ class ApplicationController < Sinatra::Base
     erb :welcome
   end
 
+  helpers do
+    def logged_in?
+      !!current_user
+    end
+
+    def current_user
+      user ||= User.find_by_id(session[:user_id]) if session[:user_id]
+    end
+  end
 end
