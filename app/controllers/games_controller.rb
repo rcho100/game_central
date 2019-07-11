86a93272dd60153dc93306e246f7ed37ba7a5bab
github username <github email address>
2019-07-11 00:23:17 -0400

    Add index action get '/games' and create index view

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,3 +1,10 @@
 class GamesController < ApplicationController
-
+  get '/games' do
+    if logged_in?
+      @games = Game.all
+      erb :'/games/index'
+    else
+      redirect to "/login"
+    end
+  end
 end
