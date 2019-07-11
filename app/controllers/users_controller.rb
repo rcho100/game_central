5ac95ccb777c22eef514b102c6a99d287f308c4e
github username <github email address>
2019-07-10 23:54:20 -0400

    Add post login action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -28,4 +28,15 @@ class UsersController < ApplicationController
     end
   end
 
+  post '/login' do
+    @user = User.find_by(username: params[:username])
+
+    if @user && @user.authenticate(params[:password])
+      session[:user_id] = @user.id 
+      redirect to "/games"
+    else
+      redirect to "/login"
+    end
+  end
+
 end
