48cc6ef426ce1f73e5cb60efe9aba876609f3a01
github username <github email address>
2019-07-10 23:31:04 -0400

    Add post signup action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,21 @@ class UsersController < ApplicationController
     if logged_in?
       redirect to "/games"
     else
-      erb :'/user/signup'
+      erb :'/users/signup'
     end
   end
+
+  post '/signup' do
+    if logged_in?
+      redirect to "/games"
+    else
+      if params[:username] == "" || params[:email] == "" || params[:password] == ""
+        redirect to "/signup"
+      else
+        @user = User.create(params)
+        redirect to "/login"
+      end
+    end
+  end
+
 end
