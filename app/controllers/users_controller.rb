12308d3bb62464d1815a0252d6ea991e243c5466
github username <github email address>
2019-07-12 16:30:50 -0400

    Add heading to login page & display message when password is incorrect & edit password field to hide characters typed in

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -41,10 +41,11 @@ class UsersController < ApplicationController
   post '/login' do
     @user = User.find_by(username: params[:username])
 
-    if @user && @user.authenticate(params[:password])
+    if @user.authenticate(params[:password])
       session[:user_id] = @user.id 
       redirect to "/games"
     else
+      flash[:incorrect] = "Incorrect password. Please try again."
       redirect to "/login"
     end
   end
