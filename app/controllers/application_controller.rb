f014d0de0b64d34d08d751a5c152be67c6bc5304
github username <github email address>
2019-07-16 17:36:03 -0400

    Add helper method to redirect if not logged in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -21,5 +21,11 @@ class ApplicationController < Sinatra::Base
     def current_user
       user ||= User.find_by_id(session[:user_id]) if session[:user_id]
     end
+
+    def redirect_if_not_logged_in
+      if !logged_in?
+        redirect to "/login"
+      end
+    end
   end
 end
